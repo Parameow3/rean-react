06118d50d74116a4f7afac04e9823136f67ea3a3
Tan Bunnchhay <bunchay1280@gmail.com>
2023-03-02 10:50:57 +0700

    test CI/CD

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Menghun spinning for the life time xD
+          Test CICD
         </p>
         <a
           className="App-link"
