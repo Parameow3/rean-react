367173347781ceb11a85f33981c132f4fe36c8d0
Tan Bunnchhay <bunchay1280@gmail.com>
2023-03-02 10:13:27 +0700

    day 1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './p.svg';
 import './App.css';
 
 function App() {
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Menghun spinning for the life time xD
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Parameow3"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn More
         </a>
       </header>
     </div>
